15701fc662cc6d161e0b4cdc9e77bbf53ee36799
Jonyelson Lopes <jonyelson20@gmail.com>
2022-12-26 13:01:56 -0300

    feat: page header and application global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import "./App.css";
+import { Banner } from "./components/Banner/Banner";
+import "./global.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1>Bem vindo React com Alura</h1>;
+  return <Banner />;
 }
 
 export default App;
