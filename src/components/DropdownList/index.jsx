3a4855548150893fb7d0e4ff1c36fb6e7141101d
Jonyelson Lopes <jonyelson20@gmail.com>
2022-12-27 00:30:20 -0300

    feat: controlling form inputs

--- src/components/DropdownList/index.jsx
+++ src/components/DropdownList/index.jsx
@@ -4,7 +4,11 @@ export function DropdownList(props) {
   return (
     <div className={styles.dropdown__list}>
       <label>{props.name}</label>
-      <select>
+      <select
+        onChange={(event) => props.handleInput(event.target.value)}
+        required={props.mandatory}
+        value={props.value}
+      >
         {props.itens.map((item) => {
           return <option key={item}>{item}</option>;
         })}
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,18 +1,53 @@
+import { useState } from "react";
+import { Button } from "../Button";
 import { DropdownList } from "../DropdownList";
 import { TextField } from "../TextField";
 import styles from "./Form.module.css";
 
 export function Form() {
+  const [name, setName] = useState("");
+  const [office, setOffice] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
   const times = ["Programacao", "Frontend", "Data Sciense", "Devops"];
+
+  const save = (event) => {
+    event.preventDefault();
+    console.log("Form foi submetido!", name, office, imagem, time);
+  };
 
   return (
     <section className={styles.container__form}>
-      <form>
+      <form onSubmit={save}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <TextField name="Nome" placeholder="Digite seu nome" />
-        <TextField name="Cargo" placeholder="Digite seu cargo" />
-        <TextField name="Imagem" placeholder="Informe o endereço da imagem" />
-        <DropdownList name="Time" itens={times} />
+        <TextField
+          value={name}
+          mandatory={true}
+          name="Nome"
+          placeholder="Digite seu nome"
+          handleInput={(value) => setName(value)}
+        />
+        <TextField
+          value={office}
+          mandatory={true}
+          name="Cargo"
+          placeholder="Digite seu cargo"
+          handleInput={(value) => setOffice(value)}
+        />
+        <TextField
+          value={imagem}
+          name="Imagem"
+          placeholder="Informe o endereço da imagem"
+          handleInput={(value) => setImagem(value)}
+        />
+        <DropdownList
+          value={time}
+          mandatory={true}
+          name="Time"
+          itens={times}
+          handleInput={(value) => setTime(value)}
+        />
+        <Button>Criar Card</Button>
       </form>
     </section>
   );
